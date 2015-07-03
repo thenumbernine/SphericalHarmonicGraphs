0c1e4ed844cac13e335d3d3412aec28640b14026
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-07-03 16:17:42 -0700

    now displays real sh

--- run.lua
+++ run.lua
@@ -4,15 +4,21 @@ local gl = require 'ffi.OpenGL'
 local GLApp = require 'glapp'
 local quat = require 'vec.quat'
 local vec3 = require 'vec.vec3'
-local sphericalHarmonic = require 'sphericalharmonics'
+local sphericalHarmonics = require 'sphericalharmonics'
 local sdl = require 'ffi.sdl'
 
-local SHApp = class(GLApp)
+local lmax = tonumber(arg[1]) or 3
+local distance = lmax
 
-local angle = 
-	quat():fromAngleAxis(0,0,1,180)
-	* quat():fromAngleAxis(1,0,0,90)
-local distance = 1
+local idiv = 360
+local jdiv = 180
+
+local angle = quat()
+--	* quat():fromAngleAxis(0,0,1,180)
+--	* quat():fromAngleAxis(1,0,0,90)
+
+
+local SHApp = class(GLApp)
 
 function SHApp:initGL()
 	gl.glEnable(gl.GL_DEPTH_TEST)
@@ -22,11 +28,6 @@ function SHApp:initGL()
 	gl.glEnable(gl.GL_LIGHT0)
 end
 		
-local lmax = tonumber(arg[1]) or 3
-
-local idiv = 360
-local jdiv = 180
-
 local vtx
 do
 	local cache = {}
@@ -38,12 +39,12 @@ do
 		
 		local phi = i/idiv*2*math.pi
 		local theta = j/jdiv*math.pi
-		local re, im = sphericalHarmonic[l][m](theta, phi)
-		local r = math.abs(re)	
+		local Y = sphericalHarmonics.real(l, m, theta, phi)
+		local r = math.abs(Y)	
 		local x = r * math.sin(theta) * math.cos(phi)
 		local y = r * math.sin(theta) * math.sin(phi)
 		local z = r * math.cos(theta)
-		local c = {re,im,x,y,z}
+		local c = {Y,x,y,z}
 		cache[l][m][i][j] = c
 		return unpack(c)
 	end
@@ -58,10 +59,10 @@ do
 		cache[l][m][i] = cache[l][m][i] or {}
 		if cache[l][m][i][j] then return unpack(cache[l][m][i][j]) end
 		
-		local ip = vec3(select(3, vtx(l,m,i+1,j)))
-		local im = vec3(select(3, vtx(l,m,i-1,j)))
-		local jp = vec3(select(3, vtx(l,m,i,j+1)))
-		local jm = vec3(select(3, vtx(l,m,i,j-1)))
+		local ip = vec3(select(2, vtx(l,m,i+1,j)))
+		local im = vec3(select(2, vtx(l,m,i-1,j)))
+		local jp = vec3(select(2, vtx(l,m,i,j+1)))
+		local jm = vec3(select(2, vtx(l,m,i,j-1)))
 		local c = -vec3.cross(ip-im,jp-jm):normalize()
 		cache[l][m][i][j] = c
 		return unpack(c)
@@ -124,14 +125,14 @@ function SHApp:update()
 			for m=-l,l do
 				print('building l='..l..' m='..m)
 				gl.glPushMatrix()
-				gl.glTranslatef(l-lmax/2,m,0)
+				gl.glTranslatef(m,lmax/2-l,0)
 				--gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
 				for i=0,idiv-1 do
 					gl.glBegin(gl.GL_TRIANGLE_STRIP)
 					for j=0,jdiv do
 						for iofs=0,1 do
-							local re, im, x, y, z = vtx(l,m,i+iofs,j)
-							if re >= 0 then
+							local Y, x, y, z = vtx(l,m,i+iofs,j)
+							if Y >= 0 then
 								gl.glColor3f(1,0,0)
 							else
 								gl.glColor3f(0,1,1)
--- sphericalharmonics.lua
+++ sphericalharmonics.lua
@@ -1,32 +1,27 @@
-require 'ext.meta'
-
-local pi = math.pi
-local sqrt = math.sqrt
-local cos = math.cos
-local sin = math.sin
-
-function constant(x)
-	return function() return x end
-end
-
-function build(r, cphi, thetaFunc)
-	return function(theta, phi)
-		return
-			r * thetaFunc(theta) * cos(cphi * phi),
-			r * thetaFunc(theta) * sin(cphi * phi)
-	end
-end
-
 local factorial = require 'factorial'
 local associatedLegendre = require 'associatedlegendre'
 
-return setmetatable({}, {__index=function(_,l)
-	return setmetatable({}, {__index=function(_,m)
-		return function(theta, phi)
-			local P = associatedLegendre(l,m,math.cos(theta))
-			local N = math.sqrt( (2*l+1)/(4*math.pi) * factorial(l-m)/factorial(l+m)  )
-			return math.cos(m * phi) * N * P, math.sin(m * phi) * N * P
-		end
-	end})
-end})
+local function complexSphericalHarmonics(l, m, theta, phi)
+	local P = associatedLegendre(l,m,math.cos(theta))
+	local N = math.sqrt( (2*l+1)/(4*math.pi) * factorial(l-m)/factorial(l+m)  )
+	return math.cos(m * phi) * N * P, math.sin(m * phi) * N * P
+end
+
+local function realSphericalHarmonics(l, m, theta, phi)
+	if m < 0 then
+		local _, im = complexSphericalHarmonics(l, math.abs(m), theta, phi)
+		return math.sqrt(2) * (-1)^m * im
+	end
+	local re, _ = complexSphericalHarmonics(l, m, theta, phi)
+	if m == 0 then
+		return re
+	else
+		return math.sqrt(2) * (-1)^m * re
+	end
+end
+
+return {
+	real = realSphericalHarmonics,
+	complex = complexSphericalHarmonics,
+}
 
