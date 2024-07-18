8bd45609588e615ef325fdfb7582e526ee2c1b1b
thenumbernine <christopher.e.moore@gmail.com>
2024-07-18 20:28:56 +0600

    switched to gles

--- run.lua
+++ run.lua
@@ -1,26 +1,24 @@
 #! /usr/bin/env luajit
-local gl = require 'gl'
+
+local cmdline = require 'ext.cmdline'.validate{
+	l = {desc='how many l levels to build'},
+	gl = {desc='which gl backend to use'},
+}(...)
+
+local table = require 'ext.table'
+local gl = require 'gl.setup'(cmdline.gl or 'OpenGL')
 local vec3d = require 'vec-ffi.vec3d'
 local sphericalHarmonics = require 'sphericalharmonics'
-local glCallOrRun = require 'gl.call'
 
-local lmax = tonumber(arg[1]) or 3
+local lmax = tonumber(cmdline.l) or 3
 
 local idiv = 360
 local jdiv = 180
 
 local SHApp = require 'glapp.orbit'():subclass()
-
+SHApp.viewUseBuiltinMatrixMath = true
 SHApp.title = 'Spherical Harmonics Graph'
 SHApp.viewDist = lmax
-
-function SHApp:initGL(...)
-	gl.glEnable(gl.GL_DEPTH_TEST)
-	gl.glEnable(gl.GL_COLOR_MATERIAL)
-	gl.glColorMaterial(gl.GL_FRONT_AND_BACK, gl.GL_DIFFUSE)
-	gl.glEnable(gl.GL_LIGHTING)
-	gl.glEnable(gl.GL_LIGHT0)
-end
 
 local vtx
 do
@@ -64,38 +62,97 @@ do
 	end
 end
 
-local list = {}
-function SHApp:update()
-	SHApp.super.update(self)
-	gl.glClear(gl.GL_COLOR_BUFFER_BIT + gl.GL_DEPTH_BUFFER_BIT)
+function SHApp:initGL(...)
+	program = require 'gl.program'{
+		version = 'latest',
+		header = 'precision highp float;',
+		vertexCode = [[
+in vec3 vertex, normal, color;
+uniform mat4 projMat, mvMat;
+out vec3 colorv, normalv;
+void main() {
+	vec4 worldposv = mvMat * vec4(vertex, 1.);
+	colorv = color;
+	normalv = normalize((mvMat * vec4(normal, 0.)).xyz);
+	gl_Position = projMat * worldposv;
+}
+]],
+		fragmentCode = [[
+in vec3 normalv, colorv;
+out vec4 fragColor;
+void main() {
+	fragColor = vec4(max(.1, abs(normalv.z)) * colorv, 1.);
+}
+]],
+	}:useNone()
 
-	glCallOrRun(list, function()
-		for l=0,lmax do
-			for m=-l,l do
-				print('building l='..l..' m='..m)
-				gl.glPushMatrix()
-				gl.glTranslatef(m,lmax/2-l,0)
-				--gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
-				for i=0,idiv-1 do
-					gl.glBegin(gl.GL_TRIANGLE_STRIP)
-					for j=0,jdiv do
-						for iofs=0,1 do
-							local vsrc = vtx(l,m,i+iofs,j)
-							if vsrc.Y >= 0 then
-								gl.glColor3f(1,0,0)
-							else
-								gl.glColor3f(0,1,1)
-							end
-							gl.glNormal3f(normal(l,m,i+iofs,j):unpack())
-							gl.glVertex3f(vsrc.vec:unpack())
+	sceneobjs = table()
+	for l=0,lmax do
+		for m=-l,l do
+			print('building l='..l..' m='..m)
+			local vofs = vec3d(m,lmax/2-l,0)
+			--gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+			for i=0,idiv-1 do
+
+				local colors = table()
+				local vertexes = table()
+				local normals = table()
+				
+				for j=0,jdiv do
+					for iofs=0,1 do
+						local vsrc = vtx(l,m,i+iofs,j)
+						if vsrc.Y >= 0 then
+							colors:append{1,0,0}
+						else
+							colors:append{0,1,1}
 						end
+						normals:append{normal(l,m,i+iofs,j):unpack()}
+						vertexes:append{(vsrc.vec + vofs):unpack()}
 					end
-					gl.glEnd()
 				end
-				gl.glPopMatrix()
+
+				sceneobjs:insert(require 'gl.sceneobject'{
+					program = program,
+					vertexes = {
+						data = vertexes,
+						count = #vertexes / 3,
+						dim = 3,
+					},
+					geometry = {
+						mode = gl.GL_TRIANGLE_STRIP,
+					},
+					attrs = {
+						color = {
+							buffer = {
+								data = colors,
+								count = #colors / 3,
+								dim = 3,
+							},
+						},
+						normal = {
+							buffer = {
+								data = normals,
+								count = #normals / 3,
+								dim = 3,
+							},
+						},
+					},
+				})
 			end
 		end
-	end)
+	end
+	
+	gl.glEnable(gl.GL_DEPTH_TEST)
+end
+
+function SHApp:update()
+	SHApp.super.update(self)
+	gl.glClear(bit.bor(gl.GL_COLOR_BUFFER_BIT, gl.GL_DEPTH_BUFFER_BIT))
+	for _,obj in ipairs(sceneobjs) do
+		obj.uniforms.mvMat = self.view.mvMat.ptr
+		obj.uniforms.projMat = self.view.projMat.ptr
+		obj:draw()
+	end
 end
 
 return SHApp():run()
